140b9d7768db69c0091d2d49a6c2087c38786ea6
Dany <dapianchiche@gmail.com>
2018-10-29 12:14:02 -0500

    xsds

--- formulario/enviar.php
+++ formulario/enviar.php
@@ -3,12 +3,12 @@
 if (isset($_POST["nombre"]) && !empty($_POST["nombre"])&&
 	isset($_POST["correo"]) && !empty($_POST["correo"])&&
 	isset($_POST["ciudad"]) && !empty($_POST["ciudad"])&&
-	//isset($_POST["contact-provincia_id"]) && !empty($_POST["contact-provincia_id"])&&
-	//isset($_POST["contact-canton_id"]) && !empty($_POST["contact-canton_id"])&&
-	//isset($_POST["contact-parroquia_id"]) && !empty($_POST["contact-parroquia_id"])&&
+	isset($_POST["contact-provincia_id"]) && !empty($_POST["contact-provincia_id"])&&
+	isset($_POST["contact-canton_id"]) && !empty($_POST["contact-canton_id"])&&
+	isset($_POST["contact-parroquia_id"]) && !empty($_POST["contact-parroquia_id"])&&
 	isset($_POST["mensaje"]) && !empty($_POST["mensaje"])) {
 
-	
+
 	return print("ok");
 
 }
