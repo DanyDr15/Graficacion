f049d00efc4eb29fd336c76e0c928f864facb4bd
Dany <dapianchiche@gmail.com>
2018-10-29 13:08:51 -0500

    dasda

--- formulario/js/enviar.js
+++ formulario/js/enviar.js
@@ -2,7 +2,17 @@ $(document).ready(function() {
 	
 	$(".formulario-contacto").bind("submit" , function(){
 
-
+		$.ajax({
+			type: $(this).attr("method"),
+			url: $(this).attr("action"),
+			data: $(this).serialize(),
+			success: function(){
+				alert("enviado");
+			},
+			error : function(){
+				alert("error");
+			}
+		});
 		return false;
 	});
 });
