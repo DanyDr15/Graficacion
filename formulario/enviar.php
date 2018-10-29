69e4e70938a1e0fee4643f9654884e12bbbb2e3c
Dany <dapianchiche@gmail.com>
2018-10-29 12:10:53 -0500

    adsd

--- formulario/enviar.php
+++ formulario/enviar.php
@@ -1,13 +1,14 @@
 <?php
 
-if(isset($_POST["nombre"]) && !empty($_POST["nombre"])&&
+if (isset($_POST["nombre"]) && !empty($_POST["nombre"])&&
 	isset($_POST["correo"]) && !empty($_POST["correo"])&&
 	isset($_POST["ciudad"]) && !empty($_POST["ciudad"])&&
 	//isset($_POST["contact-provincia_id"]) && !empty($_POST["contact-provincia_id"])&&
 	//isset($_POST["contact-canton_id"]) && !empty($_POST["contact-canton_id"])&&
 	//isset($_POST["contact-parroquia_id"]) && !empty($_POST["contact-parroquia_id"])&&
-	isset($_POST["mensaje"]) && !empty($_POST["mensaje"])
-){
+	isset($_POST["mensaje"]) && !empty($_POST["mensaje"])) {
+
+	
 	return print("ok");
 
 }
