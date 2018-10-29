a1f9723766c3ac1fb35257b9ab400533751181ac
Dany <dapianchiche@gmail.com>
2018-10-29 11:38:20 -0500

    dfgd

---
+++ formulario/enviar.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($POST["nombre"]) && !empty($_POST["nombre"])&&
+	isset($POST["correo"]) && !empty($_POST["correo"])&&
+	isset($POST["ciudad"]) && !empty($_POST["ciudad"])&&
+	isset($POST["contact-provincia_id"]) && !empty($_POST["contact-provincia_id"])&&
+	isset($POST["contact-canton_id"]) && !empty($_POST["contact-canton_id"])&&
+	isset($POST["contact-parroquia_id"]) && !empty($_POST["contact-parroquia_id"])&&
+	isset($POST["mensaje"]) && !empty($_POST["mensaje"])
+){
+	return print("ok");
+
+}
+
+return print("No se puede enviar");
