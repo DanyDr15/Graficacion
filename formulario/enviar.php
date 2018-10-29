5486a0e2947c7a4b5ac80289e6c1339dbf49f12b
Dany <dapianchiche@gmail.com>
2018-10-29 11:53:05 -0500

    fssssssssf

--- formulario/enviar.php
+++ formulario/enviar.php
@@ -3,9 +3,9 @@
 if(isset($POST["nombre"]) && !empty($_POST["nombre"])&&
 	isset($POST["correo"]) && !empty($_POST["correo"])&&
 	isset($POST["ciudad"]) && !empty($_POST["ciudad"])&&
-	isset($POST["contact-provincia_id"]) && !empty($_POST["contact-provincia_id"])&&
-	isset($POST["contact-canton_id"]) && !empty($_POST["contact-canton_id"])&&
-	isset($POST["contact-parroquia_id"]) && !empty($_POST["contact-parroquia_id"])&&
+	//isset($POST["contact-provincia_id"]) && !empty($_POST["contact-provincia_id"])&&
+	//isset($POST["contact-canton_id"]) && !empty($_POST["contact-canton_id"])&&
+	//isset($POST["contact-parroquia_id"]) && !empty($_POST["contact-parroquia_id"])&&
 	isset($POST["mensaje"]) && !empty($_POST["mensaje"])
 ){
 	return print("ok");
