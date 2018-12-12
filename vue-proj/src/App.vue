7771fd842dd01a0ff352ae1273facb3ab2b95f3f
DanyDr15 <40213455+DanyDr15@users.noreply.github.com>
2018-12-12 17:42:56 -0500

    DTG

--- vue-proj/src/App.vue
+++ vue-proj/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="BIENVENIDOSS Vue.js App"/>
+    <HelloWorld msg="BIENVENIDOS a  Vue.js App"/>
   </div>
 </template>
 
