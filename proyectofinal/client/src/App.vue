22db89c77c865cf3c30a13b3af13a468a1644025
DanyDr15 <dapianchiche@gmail.com>
2019-01-06 16:14:36 -0500

    ergt

--- proyectofinal/client/src/App.vue
+++ proyectofinal/client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">CMS DANY</a>
+      <a class="navbar-brand" href="Dashboard.vue">CMS DANY</a>
     </nav>
     <router-view class="container pt-2" />
   </div>
