1a24b5e8ad6c207651cd7e8f4005d06c912e2c83
DanyDr15 <dapianchiche@gmail.com>
2019-01-06 16:00:14 -0500

    WSRDEF

--- proyectofinal/client/src/App.vue
+++ proyectofinal/client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="#">Auth for Newbs</a>
+      <a class="navbar-brand" href="#">CMS DANY</a>
     </nav>
     <router-view class="container pt-2" />
   </div>
