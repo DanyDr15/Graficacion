e16e485c4df8eabda7c6f27a1a080f5ba585e081
DanyDr15 <40213455+DanyDr15@users.noreply.github.com>
2019-01-07 11:58:00 -0500

    sd

--- proyectofinal/client/src/App.vue
+++ proyectofinal/client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a class="navbar-brand" href="Dashboard.vue">CMS DANY</a>
+      <a class="navbar-bluebird" href="Dashboard.vue">CMS DANY</a>
     </nav>
     <router-view class="container pt-2" />
   </div>
