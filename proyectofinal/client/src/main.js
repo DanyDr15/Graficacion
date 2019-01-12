185c1de5d7ea8c092d4d329c2f1de2e7f5daed0d
DanyDr15 <40213455+DanyDr15@users.noreply.github.com>
2019-01-12 04:51:19 -0500

    corrigiendo

--- proyectofinal/client/src/main.js
+++ proyectofinal/client/src/main.js
@@ -3,10 +3,10 @@ import 'bootswatch/dist/superhero/bootstrap.css';
 
 import App from './App.vue';
 import router from './router';
-import Dashboard from './Dashboard.Vue';
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(Dashboard),
+  render: h => h(App),
 }).$mount('#app');
